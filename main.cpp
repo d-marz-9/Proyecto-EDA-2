daa61d89a0322d4dd105904c412bedc9b140a100
D. M. <dmarzal@outlook.com>
2025-07-04 14:22:07 -0500

    Mas funciones para hash

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream> //para cargar desde un archivo txt
 #include <string> //manipulacion de strings
 using namespace std;
 
@@ -39,14 +40,14 @@ public:
 };
 
 //----- USO DE HASHES -----
-class tablaHash{
+class TablaHash{
 private:
     Persona** tabla; //array dinamico de punteros | https://www.geeksforgeeks.org/cpp/creating-array-of-pointers-in-cpp/
     int tama;
     int colis; //para contar colisiones
     int elems;
 public:
-    tablaHash(int t=3000){
+    TablaHash(int t=25000){
         colis = 0; //contador inicializado
         tama = t;
         tabla = new Persona*[tama];
@@ -62,7 +63,7 @@ public:
         return r;
     }
 
-    ~tablaHash(){
+    ~TablaHash(){
         for (int i=0; i<tama; i++){
             delete tabla[i];
         }
@@ -85,6 +86,11 @@ public:
         if (intentos<tama){
             tabla[pos] = per;
             elems++;
+            //rehashing stuff
+            if (factorCarga()>0.7){
+                cout<<"Factor de carga: "<<factorCarga()<<". Realizando rehashing de manera automatica..."<<endl;
+                rehashing();
+            }
             return true;
         } else {
             cout<<"Insersion fallida. Tabla llena."<<endl;
@@ -111,6 +117,30 @@ public:
         return buscar(dni) != nullptr;
     }
 
+    void rehashing(){
+        int newTama = tama + 10000;
+        Persona** newTabla = new Persona*[newTama];
+        for (int i=0; i<newTama; i++){
+            newTabla[i] = nullptr;
+        }
+
+        int oldTama = tama;
+        Persona** oldTabla = tabla;
+
+        tabla = newTabla;
+        tama = newTama;
+        elems = 0;
+
+        for (int i=0; i<oldTama; i++){
+            if (oldTabla[i]!=nullptr){
+                insertarHS(oldTabla[i]);
+            }
+        }
+
+        delete[] oldTabla;
+        cout<<"Rehashing completo. Tamaño de nueva tabla: "<<tama<<endl;
+    }
+
     float factorCarga(){
         return float(elems)/tama;
     }
@@ -133,6 +163,60 @@ public:
 
 };
 
+void cargaDeMuchasPersonas(TablaHash* tabla, const string& listaAtendientes){ //https://cplusplus.com/doc/tutorial/files/
+    ifstream archivo(listaAtendientes);
+
+    if (!archivo.is_open()){
+        cout<<"Error critico al abrir archivo "<<listaAtendientes<<". Revisar."<<endl;
+        return;
+    }
+    int dni, prioridad;
+    string nombre, apellido;
+
+    while (archivo>>dni>>nombre>>apellido>>prioridad){
+        string zona;
+        switch (prioridad){
+            case 5:
+                zona = "VIP";
+                break;
+            case 4:
+                zona = "Personal Medico";
+                break;
+            case 3:
+                zona = "Personal Seguridad";
+                break;
+            case 2:
+                zona = "Discapacitados";
+                break;
+            case 1:
+                zona = "Publico general";
+                break;
+            default:
+                zona = "Unknown";
+                break;
+        }
+        Persona* per = new Persona(dni, nombre, apellido, zona, prioridad);
+        if (!tabla->insertarHS(per)){
+            delete per;
+        }
+    }
+    archivo.close();
+    cout<<"Informacion de atendientes exitoso."<<endl;
+
+    /* llamado a main, ya sabremos como se hace luego xdddd
+    int main() {
+        tablaHash* tabla = new tablaHash(3000);
+        cargarDesdeArchivo(tabla, "invitados.txt");
+
+        // Optional: test
+        tabla->printTabla();
+
+        delete tabla;
+        return 0;
+    }
+    */
+}
+
 //----- FIN DE HASHES -----
 
 //----- USO DE HEAPS -----
@@ -140,10 +224,10 @@ public:
 
 //----- FIN DE HEAPS -----
 
-//----- USO DE AVL รณ RED BLACK TREES -----
+//----- USO DE AVL ó RED BLACK TREES -----
 
 
-//----- FIN DE USO DE AVL รณ RED BLACK TREES -----
+//----- FIN DE USO DE AVL ó RED BLACK TREES -----
 
 int main()
 {
